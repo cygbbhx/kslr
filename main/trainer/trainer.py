81bb4b33e84a0e83b81637a0df2fff9e05e9a5d4
cygbbhx <cygbbhx@yonsei.ac.kr>
2023-11-17 16:47:00 +0000

    comment out printing data shape

--- main/trainer/trainer.py
+++ main/trainer/trainer.py
@@ -41,7 +41,7 @@ class Trainer(BaseTrainer):
         self.train_metrics.reset()
         for batch_idx, (data, target) in enumerate(self.data_loader):
             data, target = data.to(self.device), target.to(self.device)
-            print(data.shape)
+            #print(data.shape)
 
             self.optimizer.zero_grad()
             output = self.model(data)
