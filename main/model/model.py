c625004b5963a04438e21d543c6f6497260b263d
jmjmfasdf <104200033+jmjmfasdf@users.noreply.github.com>
2023-11-18 16:09:56 +0900

    Update model.py - added 1dcnn-lstm and lstm model

--- main/model/model.py
+++ main/model/model.py
@@ -53,3 +53,82 @@ class P3D(BaseModel):
     def forward(self, x):
         x = self.model(x)
         return F.log_softmax(x, dim=1)
+
+class Conv1d_LSTM(nn.Module):
+    def __init__(self,in_channels=128, num_classes=60):
+        super(Conv1d_LSTM2, self).__init__()
+        # Adjusted in_channels to 137 to match dataset output
+        self.conv1d_1 = nn.Conv1d(in_channels=in_channels, 
+                                  out_channels=16, 
+                                  kernel_size=5, 
+                                  stride=1, 
+                                  padding=1)
+        self.conv1d_2 = nn.Conv1d(in_channels=16, 
+                                  out_channels=32, 
+                                  kernel_size=5, 
+                                  stride=1, 
+                                  padding=1)
+        
+        self.lstm = nn.LSTM(input_size=32, 
+                            hidden_size=50, 
+                            num_layers=1, 
+                            bias=True, 
+                            bidirectional=False, 
+                            batch_first=True)
+        
+        self.dropout = nn.Dropout(0.5)
+
+        self.dense1 = nn.Linear(50, 32)
+        # Adjusted out_features to num_classes
+        self.dense2 = nn.Linear(32, num_classes)
+
+    def forward(self, x):
+        # No need to reshape x as it already matches the expected shape
+        # Proceed with 1D convolutions
+        x = self.conv1d_1(x)
+        x = self.conv1d_2(x)
+        # Transpose to match LSTM input requirements
+        x = x.transpose(1, 2)
+
+        self.lstm.flatten_parameters()
+        _, (hidden, _) = self.lstm(x)
+        x = hidden[-1]
+
+        x = self.dropout(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+
+        return F.log_softmax(x, dim=1)
+
+
+class LSTMmodel(nn.Module):
+    def __init__(self,in_channels=128, num_classes=60):
+        super(LSTMmodel, self).__init__()
+        self.lstm = nn.LSTM(input_size=in_channels, 
+                            hidden_size=50, 
+                            num_layers=1, 
+                            bias=True, 
+                            bidirectional=False, 
+                            batch_first=True)
+        
+        self.dropout = nn.Dropout(0.5)
+        self.dense1 = nn.Linear(50, 32)
+        self.dense2 = nn.Linear(32, num_classes)
+
+    def forward(self, x):
+        lstm_out, (hidden, _) = self.lstm(x)
+
+        # Optionally, you can use the output of the last time step from LSTM for further processing
+        # For instance, if you're interested in the last time step's output:
+        # x = lstm_out[:, -1, :]
+
+        # If you want to use the last hidden state (which is common in many applications):
+        x = hidden[-1]
+
+        # Pass through dropout layer
+        x = self.dropout(x)
+
+        # Pass through fully connected layers
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return F.log_softmax(x, dim=1)
