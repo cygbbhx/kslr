974f652d629cfe11cd142ad1a18eda078b8c457e
cygbbhx <cygbbhx@yonsei.ac.kr>
2023-12-05 02:51:29 +0000

    add CNNLSTM

--- main/model/model.py
+++ main/model/model.py
@@ -5,6 +5,10 @@ from torchvision import models
 from model.i3d import InceptionI3d
 from model.p3d import P3D199
 import torch
+import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn.functional as F
+from torchvision.models import resnet18, resnet101
 
 class ResNet18(BaseModel):
     def __init__(self, num_classes=60):
@@ -34,6 +38,28 @@ class P3D(BaseModel):
 
     def forward(self, x):
         x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
+
+class CNNLSTM(nn.Module):
+    def __init__(self, num_classes=5):
+        super(CNNLSTM, self).__init__()
+        self.resnet = resnet101(pretrained=True)
+        self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, 300))
+        self.lstm = nn.LSTM(input_size=300, hidden_size=256, num_layers=3)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+       
+    def forward(self, x_3d):
+        hidden = None
+        for t in range(x_3d.size(1)):
+            with torch.no_grad():
+                x = self.resnet(x_3d[:, t, :, :, :])  
+            out, hidden = self.lstm(x.unsqueeze(0), hidden)         
+
+        x = self.fc1(out[-1, :, :])
+        x = F.relu(x)
+        x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
 class Conv1d_LSTM(nn.Module):
